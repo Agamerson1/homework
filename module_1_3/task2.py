d68e854e5e8fc8f151c37a40b204e7c0b0e27ffa
Max <agamerson1@gmail.com>
2024-10-06 16:35:15 +0700

    05.10

--- module_1_3/task2.py
+++ module_1_3/task2.py
@@ -2,5 +2,5 @@ amt_completed_tasks = 12
 amt_spent_hours = 1.5
 course_name = 'Python'
 time_for_1_task = (amt_spent_hours / amt_completed_tasks)
-print('Курс:', course_name, ',' 'всего задач:', amt_completed_tasks, ',' 'затрачено часов:', amt_spent_hours,
-      ',' 'среднее время выполнения: ', time_for_1_task)
+print('Курс:', course_name + ', ' 'всего задач:', (str(amt_completed_tasks)) + ', ' 'затрачено часов:',
+      (str(amt_spent_hours)) + ', ' 'среднее время выполнения: ', time_for_1_task)
