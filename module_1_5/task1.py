8ac1232b34d0c82d9568f60ba986218a7afcb473
Max <agamerson1@gmail.com>
2024-10-08 20:35:53 +0700

    05.10

--- module_1_5/task1.py
+++ module_1_5/task1.py
@@ -1,7 +1,6 @@
 immutable_var = 1, True, "moonlight", 10.1
 print(immutable_var)
-# immutable_var [0] = 3  #Кортеж- неизменяемая упорядоченная коллекция, включающая в себя разные типы данных.'tuple' object does not support item assignment.
-# print(immutable_var)
+# immutable_var [0] = 3  #Кортеж- неизменяемая упорядоченная коллекция, которая может включать в себя разные типы данных. Ошибка при попытке изменить кортеж- 'tuple' object does not support item assignment.
 mutable_list = [2, False, "sunlight"]
 mutable_list[0] = 1000
 print(mutable_list)
