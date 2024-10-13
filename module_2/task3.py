823c18b145483d816eb9aa59625db1c05445b5f9
Max <agamerson1@gmail.com>
2024-10-13 13:12:39 +0700

    13.10

--- module_2/task3.py
+++ module_2/task3.py
@@ -1,4 +1,9 @@
 my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
-number = 0
-while len(number) < 11:
-    print(number)
+value = 0
+while value < len(my_list):
+    number = my_list[value]
+    value = value + 1
+    # print (number)
+    if number == 0:
+        break
+print(number)
