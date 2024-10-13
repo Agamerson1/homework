99c11f3ab8419dcfa8f261dea890840cc02456d2
Max <agamerson1@gmail.com>
2024-10-13 11:54:54 +0700

    13.10

--- module_2/task2.py
+++ module_2/task2.py
@@ -1,5 +1,5 @@
 first = int(input("Введите первое целое число: "))
-second = int(input("Введите второе целое число: "))
+second = int(input("Введите второе целое число : "))
 third = int(input("Введите третье целое число: "))
 if first == second == third:
     print(3)
