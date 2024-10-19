5b31e4aa347babb82f00d113b955dc7a81cd3b67
Max <agamerson1@gmail.com>
2024-10-19 16:24:01 +0700

    17.10

--- module_2/task4.py
+++ module_2/task4.py
@@ -1,7 +1,15 @@
 numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
 primes = []
 not_primes = []
-for i in numbers:
+for i in range(len(numbers)):
+    if i < 2:
+        continue
+    is_prime = True
     # print(i)
-    if i % 1 == 0 and i % i == 0 and i % 2 != 0 and i % 3 != 0:
-        primes.append
+    if i % 1 == 0 and i % i == 0 and i % 2 != 0 and i % 15 != 0:
+        primes.append(i)
+    else:
+        if not (is_prime):
+            not_primes.append(i)
+print(primes)
+print(not_primes)
