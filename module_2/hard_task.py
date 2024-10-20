cd8c23b4e69b677ea30db49340f6e7cfc42ea5a3
Max <agamerson1@gmail.com>
2024-10-20 12:57:16 +0700

    20.10

---
+++ module_2/hard_task.py
@@ -0,0 +1,7 @@
+import random
+
+
+def get_insert():
+    numbers = [3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 18, 19, 20]
+    num = list(range(1, 20))
+    insert = random.choice(numbers)
