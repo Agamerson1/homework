f4c5579aae91cb6392fefb9ce0269cbd720a29da
Max <agamerson1@gmail.com>
2024-10-21 19:32:44 +0700

    21.10

--- module_3/task1.py
+++ module_3/task1.py
@@ -7,6 +7,24 @@ def count_calls():
 
 
 def string_info(string):
-    cttring = len(string), string.upper(), string.lower()
+    sttring = str(string)
+    tuple = len(string), string.upper(), string.lower()
+    count_calls()
+    return tuple
 
-def is_contains():
+
+def is_contains(string, list_to_search):
+    string = string.lower
+    list_to_search = list(list_to_search)
+    for i in list_to_search:
+
+
+print(string_info('Capybara'))
+
+print(string_info('Armageddon'))
+
+print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN']))  # Urban ~ urBAN
+
+print(is_contains('cycle', ['recycling', 'cyclic']))  # No matches
+
+print(calls)
