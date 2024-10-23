45bdba90144f3b016843140fb5404a4925e0c089
Max <agamerson1@gmail.com>
2024-10-23 20:30:48 +0700

    23.10

--- module_3/task3.py
+++ module_3/task3.py
@@ -5,4 +5,9 @@ def print_params(a=1, b='строка', c=True):
 print_params()
 print_params(b=25)
 print_params(c=[1, 2, 3])
-values = [10, 'десять', False]
+values_list = [10, 'десять', False]
+values_dict = {'a': 11, 'b': 'одиннадцать', 'c': False}
+print_params(*values_list)
+print_params(**values_dict)
+values_list_2 = [3.14, 'Пи']
+print_params(*values_list_2, 42)
