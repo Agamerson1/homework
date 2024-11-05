b9b654b4dea959930f221c470efcb72d671028b4
Max <agamerson1@gmail.com>
2024-11-05 21:10:17 +0700

    05.11

--- module_3/hard_task.py
+++ module_3/hard_task.py
@@ -1,8 +1,16 @@
 def calculate_structure_sum(data_structure):
     sum = 0
     for i in data_structure:
-        if isinstance(i, int)Ж
-        sum += 1
+        if isinstance(i, int):
+            sum += i
+        elif isinstance(i, str):
+            sum += len(i)
+        elif isinstance(i, dict):
+            for key, value in dict:
 
+
+data_structure = [[1, 2, 3], {'a': 4, 'b': 5}, (6, {'cube': 7, 'drum': 8}), "Hello",
+                  ((), [{(2, 'Urban', ('Urban2', 35))}])]
+result = calculate_structure_sum(data_structure)
 
 print(result)
