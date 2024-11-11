967ec7b47ccad7c7a403948577379acf6aa146a4
Max <agamerson1@gmail.com>
2024-11-11 11:44:25 +0700

    11.11

--- module_5/task1.py
+++ module_5/task1.py
@@ -8,5 +8,11 @@ class House:
         if new_floor > self.number_of_floors or new_floor < 1:
             print('Такого этажа не существует')
         else:
-            for floor in range(new_floor):
-                print(cur_floor)
+            for cur_floor in range(new_floor):
+                print(cur_floor + 1)
+
+
+h1 = House('ЖК Победа', 300)
+h2 = House('Гараж', 2)
+h1.go_to(7)
+h2.go_to(-1)
