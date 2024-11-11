1fcf8fc7a128b9a22eb937eabcef76c1b4a0ec55
Max <agamerson1@gmail.com>
2024-11-11 11:40:07 +0700

    11.11

---
+++ module_5/task1.py
@@ -0,0 +1,12 @@
+class House:
+    def __init__(self, name, number_of_floors):
+        self.name = name
+        self.number_of_floors = number_of_floors
+
+    def go_to(self, new_floor):
+        cur_floor = 0
+        if new_floor > self.number_of_floors or new_floor < 1:
+            print('Такого этажа не существует')
+        else:
+            for floor in range(new_floor):
+                print(cur_floor)
