dadaccadcad8d9a1b7da02d599f6486f03f80575
Max <agamerson1@gmail.com>
2024-11-20 13:32:59 +0700

    20.11

--- module_5/hard_task.py
+++ module_5/hard_task.py
@@ -1,5 +1,35 @@
 class User:
     def __init__(self, nickname, password, age):
         self.nickname = nickname
-        self.password = password
+        self.password = self.hash_password(password)
         self.age = age
+
+    def hash_password(self, password):
+        password = hash(password)
+
+
+class Video:
+    def __init__(self, title, duration, time_now=0, adult_mode=False):
+        self.title = title
+        self.duration = duration
+        self.time_now = time_now
+        self.adult_mode = adult_mode
+
+
+class UrTube:
+    def __init__(self, users, videos, current_user):
+        self.users = users
+        self.videos = videos
+        self.current_user = current_user
+
+    def log_in(self, nickname, password):
+        pass
+
+    def register(self, nickname, password, age):
+        pass
+
+    def log_out(self, nickname):
+        current_user = None
+
+    def __add__(self, other):
+        videos += self
