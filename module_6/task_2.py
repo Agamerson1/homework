4e9dedd529ff79ce7d7851267584a6fe73ceaf5a
Max <agamerson1@gmail.com>
2024-11-27 12:10:43 +0700

    27.11

--- module_6/task_2.py
+++ module_6/task_2.py
@@ -7,11 +7,26 @@ class Vehicle:
         self.__engine_power = engine_power
         self.__color = color
 
-    def get_model(self, model):
+    def get_model(self):
         return f'Модель: {self.__model}'
 
-    def get_horsepower(self, engine_power):
+    def get_horsepower(self):
+        return f'Мощность двигателя: {self.__engine_power}'
 
+    def get_color(self):
+        return f'Цвет: {self.color}'
+
+    def print_info(self):
+        print(f'Владелец: {self.owner}')
+        print(self.get_model())
+        print(self.get_horsepower())
+        print(self.get_color())
+
+    def set_color(self, new_color):
+        if new_color in Vehicle.__COLOR_VARIANTS:
+            self.__color = new_color
+        else:
+            print(f'Нельзя сменить цвет на {new_color}')
 
 class Sedan(Vehicle):
     def __init__(self):
