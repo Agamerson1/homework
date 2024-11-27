617594e6f134b6920b220f8581bc7e737cb071b6
Max <agamerson1@gmail.com>
2024-11-27 12:00:42 +0700

    27.11

--- module_6/task_2.py
+++ module_6/task_2.py
@@ -1,9 +1,16 @@
 class Vehicle:
+    __COLOR_VARIANTS = ['pink', 'red', 'black', 'green', 'yellow']
+
     def __init__(self, owner, model, engine_power, color):
         self.owner = owner
-        self.model = model
-        self.engine_power = engine_power
-        self.color = color
+        self.__model = model
+        self.__engine_power = engine_power
+        self.__color = color
+
+    def get_model(self, model):
+        return f'Модель: {self.__model}'
+
+    def get_horsepower(self, engine_power):
 
 
 class Sedan(Vehicle):
