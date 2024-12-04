3f5b5a0629745adaa1669cc307854b9e0cfaf088
Max <agamerson1@gmail.com>
2024-12-04 21:25:36 +0700

    04.12

---
+++ module_7/task_1.py
@@ -0,0 +1,38 @@
+class Product:
+    def __init__(self, name, weight, category):
+        self.name = str(name)
+        self.weight = float(weight)
+        self.category = str(category)
+
+    def __str__(self):
+        return f'Продукт: {self.name}, вес: {self.weight} кг, категория: {self.category}'
+
+
+class Shop:
+    __file_name = 'products.txt'
+
+    def get_products(self):
+        file = open(self.__file_name, 'r')
+        products = file.readlines()
+        file.close()
+        return products
+
+    def add(self, *products):
+        file_get = self.get_products()
+        for i in products:
+            prod_str = str(i)
+            if i not in file_get:
+                file = open(self.__file_name, 'a')
+                file.write(prod_str + '\n')
+                file.close()
+            else:
+                print(f'Продукт "{i.name}" уже есть в магазине.')
+
+
+s1 = Shop()
+p1 = Product('Potato', 50.5, 'Vegetables')
+p2 = Product('Spaghetti', 3.4, 'Groceries')
+p3 = Product('Potato', 5.5, 'Vegetables')
+print(p2)  # __str__
+s1.add(p1, p2, p3)
+print(s1.get_products())
