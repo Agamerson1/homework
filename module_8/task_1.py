0d192f6033e02329a3fd9329d05235b4f04c9121
Max <agamerson1@gmail.com>
2024-12-13 13:32:35 +0700

    13.12

---
+++ module_8/task_1.py
@@ -0,0 +1,13 @@
+def add_everything_up(a, b):
+    try:
+        return a + b
+    except TypeError:
+        if type(a) == int or float and type(b) == str:
+            return str(a) + b
+        elif type(a) == str and type(b) == int or float:
+            return a + str(b)
+
+
+print(add_everything_up(123.456, 'строка'))
+print(add_everything_up('яблоко', 4215))
+print(add_everything_up(123.456, 7))
