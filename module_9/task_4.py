1d52ae673d4b3e14891dc8edf2cb06a90786ad5a
Max <agamerson1@gmail.com>
2024-12-22 16:10:21 +0700

    22.12

---
+++ module_9/task_4.py
@@ -0,0 +1,37 @@
+import random
+
+# lambda
+first = 'Мама мыла раму'
+second = 'Рамена мало было'
+conc = list(map(lambda a, b: a == b, first, second))
+print(conc)
+
+
+# замыкание
+def get_advanced_writer(file_name):
+    def write_everything(*data_set):
+        with open(file_name, 'w', encoding='utf-8') as file:
+            for item in data_set:
+                file.write(f'{item}\n')
+
+    return write_everything
+
+
+write = get_advanced_writer('example.txt')
+write('Это строчка', ['А', 'это', 'уже', 'число', 5, 'в', 'списке'])
+
+
+# __call__
+class MysticBall:
+    def __init__(self, *words):
+        self.words = words
+
+    def __call__(self):
+        return random.choice(self.words)
+
+
+first_ball = MysticBall('Сегодня', 'Завтра', 'Вчера', 'Никогда')
+
+print(first_ball())
+print(first_ball())
+print(first_ball())
