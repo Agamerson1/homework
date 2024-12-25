e73e2e6a0d7c8a5b1d9e8429a9da3dfa8540f71f
Max <agamerson1@gmail.com>
2024-12-25 11:50:12 +0700

    25.12

--- module_9/task_6.py
+++ module_9/task_6.py
@@ -1,7 +1,10 @@
-def all_variants(text):
-    text = text.product
-    yield all_variants(text)
+import itertools
 
+def all_variants(text):
+    length = len(text)
+    for a in range(length):
+        for b in range(a, length):
+            yield text[a:b + 1]
 
 a = all_variants("abc")
 for i in a:
