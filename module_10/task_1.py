b3b27e1ee1d19890edaa9830d04280d15e4ce645
Max <agamerson1@gmail.com>
2024-12-30 12:57:57 +0700

    30.12

--- module_10/task_1.py
+++ module_10/task_1.py
@@ -5,5 +5,12 @@ import threading
 def wite_words(word_count, file_name):
     with open(file_name, 'w', encoding='utf-8'):
         for i in range(word_count):
-            i += 1
-            print(f'Какое-то слово № {i}')
+            print(f'Какое-то слово № {i + 1}')
+            time.sleep(0.1)
+        print(f'Завершилась запись в файл {file_name}')
+
+
+time_start = time.time()
+thread = threading.Thread(target=wite_words)
+thread.start()
+thread.join()
