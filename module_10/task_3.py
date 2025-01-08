47948c952f393066c8df0d7186d0151145afaa35
Max <agamerson1@gmail.com>
2025-01-09 01:34:08 +0700

    08.01

--- module_10/task_3.py
+++ module_10/task_3.py
@@ -12,10 +12,24 @@ class Bank:
 
     def deposit(self):
         for i in range(100):
-            self.balance += random.randint(50, 100)
+            random_num = random.randint(50, 100)
             if self.balance >= 500 and lock.locked:
                 lock.release()
-            print(f'Пополнение: {random.randint(50, 100)}')
+            self.balance += random_num
+            print(f'Пополнение: {random_num}. Баланс: {self.balance}')
             time.sleep(0.001)
 
     def take(self):
+        for i in range(100):
+            random_num = random.randint(50, 100)
+            print(f'Запрос на {random_num}')
+            if random_num == self.balance or random_num <= self.balance:
+                self.balance -= random_num
+                print(f'Снятие: {random_num}. Баланс: {self.balance}')
+            else:
+                print('Запрос отклонён, недостаточно средств на счету')
+                lock.acquire()
+            time.sleep(0.001)
+
+
+bk = Bank(0, lock)
